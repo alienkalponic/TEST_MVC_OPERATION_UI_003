ad4041e24500e8cf641f189efd3449051660035f
Arnab Das <arnabdas90job@outlook.com>
2020-12-05 22:08:14 +0530

    Add project files.

---
+++ TEST_MVC_OPERATION_UI_003/App_Start/Constant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TEST_MVC_OPERATION_UI_003.App_Start
+{
+    public class Constant
+    {
+    }
+}
---
+++ TEST_MVC_OPERATION_UI_003/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TEST_MVC_OPERATION_UI_003.MvcApplication" Language="C#" %>
